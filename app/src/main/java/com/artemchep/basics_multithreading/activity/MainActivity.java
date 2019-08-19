5ea87281151c27e43cbdf63be0fe41703e806f9b
yevhenii.kachanov <Hesoyam133>
2019-08-19 17:55:14 +0300

    1d Commit start it

---
+++ app/src/main/java/com/artemchep/basics_multithreading/activity/MainActivity.java
@@ -0,0 +1,124 @@
+package com.artemchep.basics_multithreading.activity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+import android.view.View;
+
+import androidx.annotation.UiThread;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.artemchep.basics_multithreading.R;
+import com.artemchep.basics_multithreading.adapter.MessageAdapter;
+import com.artemchep.basics_multithreading.cipher.CipherUtil;
+import com.artemchep.basics_multithreading.domain.Message;
+import com.artemchep.basics_multithreading.domain.WithMillis;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int SENT_TO_THREAD = 24;
+    private static final int FROM_UI_THREAD = 42;
+
+    private List<WithMillis<Message>> listOfMessage = new ArrayList<>();
+
+    private MessageAdapter mAdapter = new MessageAdapter(listOfMessage);
+
+    private HandlerThread bgThread = new HandlerThread("bgThread");
+    private Handler bgThreadHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(mAdapter);
+
+//        showWelcomeDialog();
+
+        bgThread.start();
+        bgThreadHandler = new Handler(bgThread.getLooper(), backgroundThreadCallback);
+    }
+
+    @Override
+    protected void onDestroy() {
+        bgThread.quit();
+        super.onDestroy();
+    }
+
+    private void showWelcomeDialog() {
+        new AlertDialog.Builder(this)
+                .setMessage("What are you going to need for this task: Thread, Handler.\n" +
+                        "\n" +
+                        "1. The main thread should never be blocked.\n" +
+                        "2. Messages should be processed sequentially.\n" +
+                        "3. The elapsed time SHOULD include the time message spent in the queue.")
+                .show();
+    }
+
+    public void onPushBtnClick(View view) {
+        Message message = Message.generate();
+        insert(new WithMillis<>(message));
+    }
+
+    @UiThread
+    public void insert(final WithMillis<Message> message) {
+        listOfMessage.add(message);
+        mAdapter.notifyItemInserted(listOfMessage.size() - 1);
+        final WithMillis<Message> messageWithStartTime = new WithMillis<>(message.value, System.currentTimeMillis());
+        bgThreadHandler.obtainMessage(SENT_TO_THREAD, messageWithStartTime).sendToTarget();
+    }
+
+    @UiThread
+    public void update(final WithMillis<Message> message) {
+        for (int i = 0; i < listOfMessage.size(); i++) {
+            if (listOfMessage.get(i).value.key.equals(message.value.key)) {
+                listOfMessage.set(i, message);
+                mAdapter.notifyItemChanged(i);
+                return;
+            }
+        }
+
+        throw new IllegalStateException();
+    }
+
+    private final Handler mainThreadHandler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(android.os.Message msg) {
+            if (msg.what == FROM_UI_THREAD) {
+                update((WithMillis<Message>) msg.obj);
+            }
+        }
+    };
+
+    private final Handler.Callback backgroundThreadCallback = new Handler.Callback() {
+        @Override
+        public boolean handleMessage(android.os.Message msg) {
+            if (msg.what == SENT_TO_THREAD) {
+
+                WithMillis<Message> messageWithStartTime = (WithMillis<Message>) msg.obj;
+                Message message = messageWithStartTime.value;
+                long time = messageWithStartTime.elapsedMillis;
+
+
+                final Message messageNew = message.copy(CipherUtil.encrypt(message.plainText));
+                long elapsedTime = System.currentTimeMillis() - time;
+
+                final WithMillis<Message> messageNewWithMillis = new WithMillis<>(messageNew, elapsedTime);
+
+                mainThreadHandler.obtainMessage(FROM_UI_THREAD, messageNewWithMillis).sendToTarget();
+
+                return true;
+            }
+            return false;
+        }
+    };
+}
